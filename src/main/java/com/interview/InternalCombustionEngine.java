7e88a5862feca6e32a812a7111f2672fb35bbbf2
shariffdinah <shariff@protonmail.com>
2015-12-08 00:22:31 +0000

    issue with Git , have to checkin again

--- src/main/java/com/interview/InternalCombustionEngine.java
+++ src/main/java/com/interview/InternalCombustionEngine.java
@@ -1,49 +1,16 @@
 package com.interview;
 
-public class InternalCombustionEngine {
-
-    private boolean running;
-    private int fuelLevel;
-    private FuelType requiredFuelType;
-    private FuelType fuelType;
+public class InternalCombustionEngine extends Engine {
 
     public InternalCombustionEngine(FuelType requiredFuelType) {
+		
         this.requiredFuelType = requiredFuelType;
         running = false;
         fuelLevel = 0;
     }
 
-    public void start() {
-        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
-            running = true;
-        } else {
-            throw new IllegalStateException("Not able to start engine.");
-        }
-    }
+	public int getBatchSize(){
+		return 8;
+	}
 
-    public void stop() {
-        running = false;
-    }
-
-    public boolean isRunning() {
-        return running;
-    }
-
-    public void fill(FuelType fuelType, int fuelLevel) {
-        if (fuelLevel >= 0 && fuelLevel <= 100) {
-            this.fuelLevel = fuelLevel;
-        }
-        else if (fuelLevel > 100) {
-            this.fuelLevel = 100;
-        }
-        else {
-            this.fuelLevel = 0;
-        }
-
-        this.fuelType = fuelType;
-    }
-
-    public FuelType getFuelType() {
-        return  requiredFuelType;
-    }
 }
