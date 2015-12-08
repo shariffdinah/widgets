15452fa5cb4533b140873023d5c09ef7d78a31ee
shariffdinah <shariff@protonmail.com>
2015-12-08 00:21:56 +0000

    added new method for batch size

---
+++ src/main/java/com/interview/Engine.java
@@ -0,0 +1,46 @@
+package com.interview;
+
+public abstract class Engine {
+
+    protected boolean running;
+    protected int fuelLevel;
+    protected FuelType requiredFuelType;
+    protected FuelType fuelType;
+
+    public void start() {
+        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
+            running = true;
+        } else {
+            throw new IllegalStateException("Not able to start engine.");
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void fill(FuelType fuelType, int fuelLevel) {
+		
+        if (fuelLevel >= 0 && fuelLevel <= 100) {
+            this.fuelLevel = fuelLevel;
+        }
+        else if (fuelLevel > 100) {
+            this.fuelLevel = 100;
+        }
+        else {
+            this.fuelLevel = 0;
+        }
+
+        this.fuelType = fuelType;
+    }
+
+    public FuelType getFuelType() {
+        return  requiredFuelType;
+    }
+	
+	public abstract int getBatchSize();
+}
